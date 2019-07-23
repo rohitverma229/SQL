a38bb6d01060820dac5a2e1dc2e64d3923318b83
Rohit Verma <48760327+rohitverma229@users.noreply.github.com>
2019-07-23 19:17:10 +0530

    updated question6

--- query_6.sql
+++ query_6.sql
@@ -1,20 +1,20 @@
-create table category
-(
-category_id int auto_increment primary key,
-name varchar(20) not null,
-parent INT DEFAULT NULL
+-- Query that gives immidiate cild from the master(parent) node
+
+create table nested_category (
+ category_id int auto_increment primary key,
+ name varchar(20) not null,
+ lft int not null,
+ rgt int not null
 );
 
-insert into category
-values
-(1,'M',NULL),(2,'C1',1),(3,'C2',1),
-(4,'C3',2),(5,'C4',2),(6,'C5',3),
-(7,'C6',3);
+insert into nested_category
+values(1,'M',1,14),(2,'C1',2,7),(3,'C2',8,13),
+(4,'C3',3,4),(5,'C4',5,6),(6,'C5',9,8),
+(7,'C6',11,12);
 
-select * from category order by category_id;
-
-select t1.name as lev1, t2.name as lev2, t3.name as lev3
-from category as t1
-left join category as t2 on t2.parent = t1.category_id
-left join category as t3 on t3.parent = t2.category_id
-where t1.name = 'M' and t3.name = 'C6';
+select node.name
+from nested_category as node,
+nested_category as parent
+where node.lft between parent.lft and parent.rgt
+and parent.name = 'C1'
+order by node.lft;
